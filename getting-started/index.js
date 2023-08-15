3c0ee508b1997b430fa528b04b65225217063f8c
Xiel0112 <guswl000112@gmail.com>
2023-08-15 21:29:56 +0900

    feat(getting-started/index): TS 파일을 JS 파일로 컴파일하기

---
+++ getting-started/index.js
@@ -0,0 +1,7 @@
+function sum2(a, b) {
+  return a + b;
+}
+sum2(10, 20);
+// COMMENT: 타입스크립트는 ts 파일을 js 파일로 변환하는 컴파일 작업 필요
+// npm i typescript -g
+// tsc 파일명
---
+++ getting-started/index.ts
@@ -0,0 +1,9 @@
+function sum2(a: number, b: number): number {
+  return a + b;
+}
+
+sum2(10, 20);
+
+// COMMENT: 타입스크립트는 ts 파일을 js 파일로 변환하는 컴파일 작업 필요
+// npm i typescript -g
+// tsc 파일명
