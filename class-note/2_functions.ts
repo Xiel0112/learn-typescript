ac4529cfc6bce97c2c2101b2d8822f1bf62bae49
Xiel0112 <guswl000112@gmail.com>
2023-08-16 13:39:00 +0900

    feat(class-note/2_functions.ts): 함수 타입 정의

---
+++ class-note/2_functions.ts
@@ -0,0 +1,17 @@
+// COMMENT: 1. 함수의 파라미터 타입 정의
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+sum(10, 20);
+
+// COMMENT: 2. 함수의 반환 값 타입 정의
+function add(): number {
+  return 10;
+}
+
+// COMMENT: 3. 함수 타입 정의
+function sum02(a: number, b: number): number {
+  return a + b;
+}
