ee8f3ab97cdbb26a173c2ac738df853bcca2c22d
Xiel0112 <guswl000112@gmail.com>
2023-08-16 13:38:37 +0900

    feat(class-note/1_type-basic.ts): 기본 타입 정의

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,20 @@
+// COMMENT: 1. 문자열
+let str: string = 'hello';
+
+// COMMENT: 2. 숫자
+let num: number = 10;
+
+// COMMENT: 3. 배열
+let arr: Array<number> = [1, 2, 3];
+let items: number[] = [1, 2, 3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+
+// COMMENT: 4. 튜플
+let address: [string, number] = ['Gangnam', 100];
+
+// COMMENT: 5. 객체
+let obj: object = {};
+let person: { name: string; age: number } = { name: 'capt', age: 100 };
+
+// COMMENT: 6. 진위값
+let show: boolean = true;
