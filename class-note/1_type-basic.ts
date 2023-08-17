3e129f9f4b87c69404217601500cf0e309c9fb06
Xiel0112 <guswl000112@gmail.com>
2023-08-17 19:09:23 +0900

    fix(class-note/1_type-basic.ts): 구분선(주석) 추가

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -1,20 +1,30 @@
 // COMMENT: 1. 문자열
 let str: string = 'hello';
 
+// --------------------------------------------------
+
 // COMMENT: 2. 숫자
 let num: number = 10;
+
+// --------------------------------------------------
 
 // COMMENT: 3. 배열
 let arr01: Array<number> = [1, 2, 3];
 let items: number[] = [1, 2, 3];
 let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
 
+// --------------------------------------------------
+
 // COMMENT: 4. 튜플
 let address: [string, number] = ['Gangnam', 100];
+
+// --------------------------------------------------
 
 // COMMENT: 5. 객체
 let obj01: object = {};
 let person: { name: string; age: number } = { name: 'capt', age: 100 };
+
+// --------------------------------------------------
 
 // COMMENT: 6. 진위값
 let show: boolean = true;
