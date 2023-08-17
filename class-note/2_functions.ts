79d2d8ca56bac908f77a7b1ba3f4a26a9987bb5d
Xiel0112 <guswl000112@gmail.com>
2023-08-17 19:11:07 +0900

    fix(class-note/2_functions.ts): 구분선(주석) 추가

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -1,15 +1,18 @@
 // COMMENT: 1. 함수의 파라미터 타입 정의
-
 function sum01(a: number, b: number) {
   return a + b;
 }
 
 sum01(10, 20);
 
+// --------------------------------------------------
+
 // COMMENT: 2. 함수의 반환 값 타입 정의
 function add(): number {
   return 10;
 }
+
+// --------------------------------------------------
 
 // COMMENT: 3. 함수 타입 정의
 function sum02(a: number, b: number): number {
@@ -18,6 +21,8 @@ function sum02(a: number, b: number): number {
 
 // FIXME: 불필요한 인자를 넘겨줄 경우, 에러 발생
 sum02(10, 20, 30, 40);
+
+// --------------------------------------------------
 
 // COMMENT: 함수의 옵셔널 파라미터
 function log(a: string, b?: string) {}
