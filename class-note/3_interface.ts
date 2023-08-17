66d3771c752034df854200e093f96f37955614e8
Xiel0112 <guswl000112@gmail.com>
2023-08-17 18:54:55 +0900

    fix(class-note/3_interface.ts): 코드 포맷 변경

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -4,37 +4,44 @@ interface User {
 }
 
 // COMMENT: 1. 변수에 인터페이스 활용
-var seho: User = {age: 33, name: '세호'}
+var seho: User = { age: 33, name: '세호' };
 
 // COMMENT: 2. 함수에 인터페이스 활용
-function getUser(user: User) {console.log(user);}
+function getUser(user: User) {
+  console.log(user);
+}
 
-const capt = {name: '캡틴', age: 100}
+const capt = { name: '캡틴', age: 100 };
 
 getUser(capt);
 
 // COMMENT: 3. 함수의 스펙(구조)에 인터페이스 활용
-interface SumFunction{(a: number, b: number): number;}
+interface SumFunction {
+  (a: number, b: number): number;
+}
 
 var sum: SumFunction;
-sum = function(a: number, b: number): number {return a + b;}
-
+sum = function (a: number, b: number): number {
+  return a + b;
+};
 
 // COMMENT: 4. 인덱싱
-interface StringArray {[index: number]: string;}
+interface StringArray {
+  [index: number]: string;
+}
 
 var arr: StringArray = ['a', 'b', 'c'];
-arr[0] = 'A'
+arr[0] = 'A';
 
 // COMMENT: 5. 딕셔너리 패턴
 interface StringRegexDictionary {
-[key: string]: RegExp;
+  [key: string]: RegExp;
 }
 
 var obj: StringRegexDictionary = {
-cssFile: /|.css$/,
-jsFile: /|.js$/,
-}
+  cssFile: /|.css$/,
+  jsFile: /|.js$/,
+};
 
 // COMMENT: 6. 인터페이스 확장
 interface Person {
@@ -47,7 +54,7 @@ interface Developer extends Person {
 }
 
 var thor: Developer = {
-  name: '캡틴', 
+  name: '캡틴',
   age: 100,
-  language: 'ts'
-}
+  language: 'ts',
+};
