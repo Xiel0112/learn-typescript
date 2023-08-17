32fe42dbd934499b0c3b03735635d6420d24d1ad
Xiel0112 <guswl000112@gmail.com>
2023-08-17 18:55:29 +0900

    fix(class-note/4_type-aliases.ts): 코드 포맷 변경

--- class-note/4_type-aliases.ts
+++ class-note/4_type-aliases.ts
@@ -1,9 +1,11 @@
 // COMMENT: interface와 type의 차이
 // 프리뷰 확인해보기
 // 타입의 확장 가능 여부 >> 인터페이스는 확장이 가능, 타입 별칭은 확장 불가능
-interface Person1 {name: string; age: number;}
-var seho: Person1 = {name: '세호', age: 30}
+interface Person1 {
+  name: string;
+  age: number;
+}
+var seho: Person1 = { name: '세호', age: 30 };
 
-type Person2 = {name: string; age: number;}
-var jinho: Person2 = {name: '진호', age: 30}
-
+type Person2 = { name: string; age: number };
+var jinho: Person2 = { name: '진호', age: 30 };
